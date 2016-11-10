efd75dcf6ea63527d13d852ff41fc46ee8a7f0ee
Donovan van Heerden <dp.vanheerden@gmail.com>
2016-11-10 06:31:09 +0200

    added tests for Todo, TodoApp, TodoList

--- app/tests/components/TodoList.test.jsx
+++ app/tests/components/TodoList.test.jsx
@@ -5,10 +5,20 @@ var $ = require('jQuery');
 var TestUtils = require('react-addons-test-utils');
 
 var TodoList = require('TodoList');
+var Todo = require('Todo');
 
 describe('TodoList', () => {
   it('should exist', () => {
     expect(TodoList).toExist();
+  });
+
+  it('should render one Todo Component for each todo item', () => {
+    var todos = [{ id: 1, text: 'Do Something' }, { id: 2, text: 'Do Something Else' }];
+
+    var todoList = TestUtils.renderIntoDocument(<TodoList todos={todos}/>);
+    var todosComponents = TestUtils.scryRenderedComponentsWithType(todoList, Todo);
+
+    expect(todosComponents.length).toBe(todos.length);
   });
 
 });
